1bcb740461a36719c120efc531f63ac23d75983b
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-11 17:40:13 -0300

    Update initial_script.sh

--- cloud/orchestrators/terraform/lesson5/initial_script.sh
+++ cloud/orchestrators/terraform/lesson5/initial_script.sh
@@ -16,12 +16,12 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 #####################################
 #  CONFIGURE DATABASE
 #####################################
-docker run --name mysql57 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=example -d mysql:5.7
+docker run -t --name mysql57 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=example -d mysql:5.7
 git clone https://github.com/leonardoreboucas/lessons.git
 cd lessons/cloud/orchestrators/sample-app/
-while [ $(netstat -nlp | grep :3306 | grep docker-proxy | wc -l) -eq 0 ]; do
-  sleep 1;
-done
+#while [ $(netstat -nlp | grep :3306 | grep docker-proxy | wc -l) -eq 0 ]; do
+#  sleep 1;
+#done
 mysql -h 127.0.0.1 -pmysql example < customer.sql
 
 #####################################
