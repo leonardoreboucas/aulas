16899778f7d4e78674cfeb31b2a27757b2c28421
Leonardo Reboucas de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-11 19:59:08 -0300

    Lesson6

---
+++ cloud/orchestrators/terraform/lesson6/blueprint.tf
@@ -0,0 +1,122 @@
+# Install a Simple CRUD app
+provider "google" {
+  credentials = file("../credentials.json")
+  project     = "reboucas-lessons"
+  region      = "us-central1"
+}
+
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
+
+resource "random_string" "random2" {
+  length = 4
+  special = false
+  upper = false
+}
+
+
+resource "random_string" "random3" {
+  length = 4
+  special = false
+  upper = false
+}
+
+resource "google_compute_instance" "give-a-name-to-your-instance" {
+  name         = "give-a-name-to-your-instance-${random_string.random.result}"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+     access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = "curl https://raw.githubusercontent.com/leonardoreboucas/lessons/master/cloud/orchestrators/terraform/lesson5/initial_script.sh | sh 2>> /var/log/start-script.log"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_instance" "give-a-name-to-your-instance-2" {
+  name         = "give-a-name-to-your-instance-${random_string.random2.result}"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+     access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = "curl https://raw.githubusercontent.com/leonardoreboucas/lessons/master/cloud/orchestrators/terraform/lesson5/initial_script.sh | sh 2>> /var/log/start-script.log"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_instance" "give-a-name-to-your-instance-3" {
+  name         = "give-a-name-to-your-instance-${random_string.random3.result}"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+     access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = "curl https://raw.githubusercontent.com/leonardoreboucas/lessons/master/cloud/orchestrators/terraform/lesson5/initial_script.sh | sh 2>> /var/log/start-script.log"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
---
+++ cloud/orchestrators/terraform/lesson6/initial_script.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+#####################################
+#  INSTALL DOCKER ENGINE
+#####################################
+apt-get update
+apt-get install -y host git apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+#####################################
+#  CONFIGURE DATABASE
+#####################################
+docker run -t --name mysql57 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=example -d mysql:5.7
+git clone https://github.com/leonardoreboucas/lessons.git
+cd lessons/cloud/orchestrators/sample-app/
+sleep 15
+docker exec -i mysql57 sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" example' < customer.sql
+
+#####################################
+#  CONFIGURE APPLICATION
+#####################################
+EXTERNAL_IP=$(host myip.opendns.com resolver1.opendns.com | grep "has address" | awk -F'address ' '{print $2}')
+docker build -t app-sample .
+docker run -p 80:80 -e DB_PASSWORD=mysql -e DB_HOST=172.17.0.2 -e DB_PORT=3306 -e HOST_IP=${EXTERNAL_IP} -d app-sample
