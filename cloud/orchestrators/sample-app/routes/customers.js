a8717ababc0308e2c0368bed5702ebc34b4e30c3
Leonardo Reboucas de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-11 18:46:15 -0300

    Updates

--- cloud/orchestrators/sample-app/routes/customers.js
+++ cloud/orchestrators/sample-app/routes/customers.js
@@ -3,6 +3,8 @@
  * GET users listing.
  */
 
+let ip = require("ip");
+
 exports.list = function(req, res){
 
   req.getConnection(function(err,connection){
@@ -13,7 +15,7 @@ exports.list = function(req, res){
             if(err)
                 console.log("Error Selecting : %s ",err );
      
-            res.render('customers',{page_title:"Customers - Node.js",data:rows});
+            res.render('customers',{page_title:"Customers - Node.js on "+ip.address(),data:rows});
                 
            
          });
