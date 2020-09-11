7ff41993df853d4363fb1024850f2ecccf463825
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-11 14:41:22 -0300

    Update initial_script.sh

--- cloud/orchestrators/terraform/lesson5/initial_script.sh
+++ cloud/orchestrators/terraform/lesson5/initial_script.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install -y git nodejs mysql-server
 git clone https://github.com/leonardoreboucas/lessons.git
-cd /cloud/orchestrators/sample-app/
+cd lessons/cloud/orchestrators/sample-app/
 npm install --no-optional
 npm start
