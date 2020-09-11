ad17741d1e945e53cc22d3fa2f5bac63002bb063
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-11 14:57:55 -0300

    Update initial_script.sh

--- cloud/orchestrators/terraform/lesson5/initial_script.sh
+++ cloud/orchestrators/terraform/lesson5/initial_script.sh
@@ -1,7 +1,20 @@
 #/bin/bash
+#####################################
+#  INSTALL DOCKER ENGINE
+#####################################
 apt-get update
-apt-get install -y git nodejs mysql-server
-git clone https://github.com/leonardoreboucas/lessons.git
-cd lessons/cloud/orchestrators/sample-app/
-npm install --no-optional
-npm start
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+#apt-get install -y git nodejs mysql-server
+#git clone https://github.com/leonardoreboucas/lessons.git
+#cd lessons/cloud/orchestrators/sample-app/
+#npm install --no-optional
+#npm start
