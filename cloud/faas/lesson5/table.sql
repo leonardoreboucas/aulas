68604b44760c85a6197c38b2704b23f248d0d491
Leonardo Reboucas de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-12 12:12:13 -0300

    update

--- cloud/faas/lesson5/table.sql
+++ cloud/faas/lesson5/table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE shifts.worker_arrival (
-	id varchar(100) auto_increment NOT NULL,
+	`id` int(11) auto_increment NOT NULL,
 	`date` TIMESTAMP NOT NULL,
-	name varchar(100) NOT NULL
+	name varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
