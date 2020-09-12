bd4a6a5f06edf9c38cf1c4616522eb658178ddf8
Leonardo Reboucas de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2020-09-12 13:03:53 -0300

    Lesson6

---
+++ cloud/faas/lesson6/addReg.js
@@ -0,0 +1,28 @@
+
+exports.addReg = (req, res) => {
+    let name = req.query.name || req.body.name || '';
+  
+    const createTcpPool = async (config) => {
+    // Extract host and port from socket address
+    const dbSocketAddr = process.env.DB_HOST.split(":")
+  
+    // Establish a connection to the database
+    let connection = await mysql.createPool({
+      user: process.env.DB_USER, // e.g. 'my-db-user'
+      password: process.env.DB_PASS, // e.g. 'my-db-password'
+      database: process.env.DB_NAME, // e.g. 'my-database'
+      host: dbSocketAddr[0], // e.g. '127.0.0.1'
+      port: dbSocketAddr[1], // e.g. '3306'
+      // ... Specify additional properties here.
+      ...config
+    });
+  }
+  
+  let sql = `INSERT INTO shifts(date, name)
+             VALUES(now(),${name})`;
+  
+  connection.query(sql);
+  connection.end();
+    res.status(200).send({result:true});
+  };
+  
